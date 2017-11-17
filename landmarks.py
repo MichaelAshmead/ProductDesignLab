5b5a7fa77e65a6ec0ef583267e6cb3eaffed3be0
Michael Ashmead <ashmeadmichael@gmail.com>
2017-11-17 11:24:30 -0500

    Got landmarks working for input image.

--- landmarks.py
+++ landmarks.py
@@ -1,4 +1,10 @@
+import cv2
+import dlib
+import numpy
+import sys
+
 PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
+SCALE_FACTOR = 1
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(PREDICTOR_PATH)
@@ -12,3 +18,26 @@ def get_landmarks(im):
         raise NoFaces
 
     return numpy.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+def annotate_landmarks(im, landmarks):
+    im = im.copy()
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+        cv2.putText(im, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    return im
+
+def read_im_and_landmarks(fname):
+    im = cv2.imread(fname, cv2.IMREAD_COLOR)
+    im = cv2.resize(im, (im.shape[1] * SCALE_FACTOR,
+                         im.shape[0] * SCALE_FACTOR))
+    s = get_landmarks(im)
+
+    return im, s
+
+im1, landmarks1 = read_im_and_landmarks(sys.argv[1])
+
+cv2.imwrite('output.jpg', annotate_landmarks(im1, landmarks1))
